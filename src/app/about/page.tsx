7e665de1356c99d80911dad5d06b2bcb1c752c41
Minseok Kim <minskim0327@gmail.com>
2023-06-16 12:43:02 +0900

    Lesson 2

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function About() {
+  // throw new Error('For Testing')
+  return (
+    <>
+    <h1>About</h1>
+    <Link href="/">Link To Home Page</Link>
+    </>
+  )
+}
